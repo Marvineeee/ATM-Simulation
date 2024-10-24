dee5567801207e8c6fdb0f67d87310492eba2b61
Marvineeee <marvinejj@gmail.com>
2024-10-23 21:33:48 -0400

    Learning Usage Of Match And Case

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ def main(balance, cash):
 def _bank_menu(balance, cash):
     menu = str(input("Display, Deposit, Withdraw, Exit: ")).strip().lower()
 
-    if menu == 'deposit':
-        amount = int(input("Deposit Amount: "))
-        deposit(amount, balance, cash)
-    elif menu == 'withdraw':
-        amount = int(input("Withdraw Amount: "))
-        withdraw(amount, balance, cash)
-    elif menu == 'display':
-        _balance_displayed = display(balance)
-        print("Bank Balance: ", _balance_displayed)
-        _bank_menu(balance, cash)
-    else:
-        print("Exiting System")
 
+    match menu:
+        case "deposit":
+            amount = int(input("Deposit Amount: "))
+            deposit(amount, balance, cash)
+        case "withdraw":
+            amount = int(input("Withdraw Amount: "))
+            withdraw(amount, balance, cash)
+        case "display":
+            _balance_displayed = display(balance)
+            print("Bank Balance: ", _balance_displayed)
+            _bank_menu(balance, cash)
+        case _:
+            print("Exiting System")
 
 def deposit(amount, balance, cash):
     if amount <= cash:
